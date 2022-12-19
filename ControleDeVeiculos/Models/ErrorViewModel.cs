6c03f2142b2ce18745e2b85965409bf30314f4d8
gilbertosantana <gilbertosantoss307@gmail.com>
2022-12-19 20:09:50 -0300

    Add project files.

---
+++ ControleDeVeiculos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ControleDeVeiculos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
